d5064fc4376e5be137e202d194ce238c4717b172
aguirrecarlos-aes <aguirre.r.carlos@gmail.com>
2026-06-24 15:28:38 -0400

    Mover feed a la pagina de inicio raiz

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { createClient } from '../../utils/supabase/server'
+import { createClient } from '../utils/supabase/server'
 
 export default async function FeedPage() {
   const supabase = await createClient()
